51ee8cbd8743c45e7d67dfb6f83104eecf99f348
pawzenn <prstiyoadi12@gmail.com>
2025-11-16 22:52:37 +0700

    push lagi pras

--- flutter_application_1/lib/modules/home/home_view.dart
+++ flutter_application_1/lib/modules/home/home_view.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
+import '../../login/login_supabase.dart';
+import '../../routes/app_routes.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  Future<void> _handleLogout() async {
+    await LoginSupabaseService.signOut();
+    Get.offAllNamed(AppRoutes.login);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lalapan Bang Ajey'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            tooltip: 'Logout',
+            onPressed: _handleLogout,
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          'Ini halaman utama setelah login.\n'
+          'Nanti bisa kamu isi dengan daftar menu, keranjang, dll.',
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
